566714947b46425b796719d1d34bed61cb82a201
Tatiana Cardona <clleidytatina@gmail.com>
2023-08-21 14:02:47 -0500

    Revert "applying recommendations runApp function"
    This reverts commit 12e2f797cd7685ae0790c2ea24364babd5c15612.

--- dist/app.js
+++ dist/app.js
@@ -1,41 +1,46 @@
 "use strict";
-const addTask = (taskList, task) => {
-    return [...taskList, task];
-};
-const completeTask = (taskList, id) => {
-    taskList.find((element) => element.id === id ? element.completed = true : element.completed = false);
-    return taskList;
-};
-const filterTask = (taskList, filter) => {
-    const newlist = taskList.filter((element) => element.completed === filter);
-    return newlist;
-};
-const printTaskList = (taskList) => {
-    taskList.map((element) => {
-        console.log(element.title, element.completed);
-    });
-};
+const list = [
+    { id: 1, title: 'Completar taller de TypeScript', completed: false },
+    { id: 2, title: 'Estudiar programación funcional', completed: false },
+    { id: 3, title: 'Preparar presentación sobre TypeScript', completed: true },
+    { id: 4, title: 'Leer libro de TypeScript avanzado', completed: false },
+    { id: 5, title: 'Resolver ejercicios de TypeScript', completed: false },
+    { id: 6, title: 'Hacer ejercicio', completed: true },
+    { id: 7, title: 'Comprar víveres', completed: false },
+    { id: 8, title: 'Llamar al médico', completed: false },
+    { id: 9, title: 'Limpiar la casa', completed: true },
+    { id: 10, title: 'Actualizar el CV', completed: false }
+];
 const runApp = () => {
-    const list = [
-        { id: 1, title: 'Completar taller de TypeScript', completed: false },
-        { id: 2, title: 'Estudiar programación funcional', completed: false },
-        { id: 3, title: 'Preparar presentación sobre TypeScript', completed: true },
-        { id: 4, title: 'Leer libro de TypeScript avanzado', completed: false },
-        { id: 5, title: 'Resolver ejercicios de TypeScript', completed: false },
-        { id: 6, title: 'Hacer ejercicio', completed: true },
-        { id: 7, title: 'Comprar víveres', completed: false },
-        { id: 8, title: 'Llamar al médico', completed: false },
-        { id: 9, title: 'Limpiar la casa', completed: true },
-        { id: 10, title: 'Actualizar el CV', completed: false }
-    ];
-    const task1 = {
-        id: 11,
-        title: "hola",
-        completed: false
+    const addTask = (taskList, task) => {
+        return [...taskList, task];
     };
-    const newlist = addTask(list, task1);
-    const completeList = completeTask(newlist, 2);
-    const filter = filterTask(completeList, true);
-    printTaskList(filter);
+    const completeTask = (taskList, id) => {
+        taskList.find((element) => element.id === id ? element.completed = true : element.completed = false);
+        return taskList;
+    };
+    const filterTask = (taskList, filter) => {
+        const newlist = taskList.filter((element) => element.completed === filter);
+        return newlist;
+    };
+    const printTaskList = (taskList) => {
+        taskList.map((element) => {
+            console.log(element.title, element.completed);
+        });
+    };
+    return {
+        addTask,
+        completeTask,
+        filterTask,
+        printTaskList
+    };
 };
-runApp();
+const task1 = {
+    id: 11,
+    title: "hola",
+    completed: false
+};
+const newlist = runApp().addTask(list, task1);
+const completeList = runApp().completeTask(newlist, 2);
+const filter = runApp().filterTask(completeList, true);
+runApp().printTaskList(filter);
--- src/app.ts
+++ src/app.ts
@@ -5,58 +5,62 @@ type Task = {
 }
 
 type TaskList = Task[];
+const list: TaskList = [
+    { id: 1, title: 'Completar taller de TypeScript', completed: false },
+    { id: 2, title: 'Estudiar programación funcional', completed: false },
+    { id: 3, title: 'Preparar presentación sobre TypeScript', completed: true },
+    { id: 4, title: 'Leer libro de TypeScript avanzado', completed: false },
+    { id: 5, title: 'Resolver ejercicios de TypeScript', completed: false },
+    { id: 6, title: 'Hacer ejercicio', completed: true },
+    { id: 7, title: 'Comprar víveres', completed: false },
+    { id: 8, title: 'Llamar al médico', completed: false },
+    { id: 9, title: 'Limpiar la casa', completed: true },
+    { id: 10, title: 'Actualizar el CV', completed: false }
+]
 
-const addTask = (taskList: TaskList, task: Task):TaskList => {
-    return [...taskList, task]
-}
-
-const completeTask = (taskList:TaskList, id:number): TaskList => {
-    taskList.find((element) =>  
-    element.id === id ? element.completed = true: element.completed=false
-    );
-
-    return taskList;
-}
-
-const filterTask = (taskList:TaskList, filter:boolean): TaskList => {
-    const newlist=taskList.filter((element) =>  
-        element.completed === filter 
-    );
-
-    return newlist;
-}
-
-const printTaskList = (taskList:TaskList):void => {
-    
-    taskList.map((element) => {
-        console.log(element.title, element.completed); 
-    })
-}
 
 
 const runApp = () => {
-    const list: TaskList = [
-        { id: 1, title: 'Completar taller de TypeScript', completed: false },
-        { id: 2, title: 'Estudiar programación funcional', completed: false },
-        { id: 3, title: 'Preparar presentación sobre TypeScript', completed: true },
-        { id: 4, title: 'Leer libro de TypeScript avanzado', completed: false },
-        { id: 5, title: 'Resolver ejercicios de TypeScript', completed: false },
-        { id: 6, title: 'Hacer ejercicio', completed: true },
-        { id: 7, title: 'Comprar víveres', completed: false },
-        { id: 8, title: 'Llamar al médico', completed: false },
-        { id: 9, title: 'Limpiar la casa', completed: true },
-        { id: 10, title: 'Actualizar el CV', completed: false }
-    ]
-
-    const task1: Task = {
-        id: 11,
-        title: "hola",
-        completed: false
+    const addTask = (taskList: TaskList, task: Task):TaskList => {
+        return [...taskList, task]
     }
-    const newlist = addTask(list,task1)
-    const completeList= completeTask(newlist,2)
-    const filter= filterTask(completeList,true)
-    printTaskList(filter);
+    
+    const completeTask = (taskList:TaskList, id:number): TaskList => {
+        taskList.find((element) =>  
+        element.id === id ? element.completed = true: element.completed=false
+        );
+    
+        return taskList;
+    }
+    const filterTask = (taskList:TaskList, filter:boolean): TaskList => {
+        const newlist=taskList.filter((element) =>  
+         element.completed === filter 
+        );
+    
+        return newlist;
+    }
+    
+    const printTaskList = (taskList:TaskList):void => {
+        
+        taskList.map((element) => {
+            console.log(element.title, element.completed); 
+        })
+    }
+
+    return {
+        addTask,
+        completeTask,
+        filterTask,
+        printTaskList
+    }
 }
 
-runApp();
+const task1: Task = {
+    id: 11,
+    title: "hola",
+    completed: false
+}
+ const newlist = runApp().addTask(list,task1)
+ const completeList= runApp().completeTask(newlist,2)
+ const filter= runApp().filterTask(completeList,true)
+ runApp().printTaskList(filter)
